8c74744d19ed9a2b6a9813e1107f25f530857d76
ihah <ernestasdev@gmail.com>
2021-11-28 16:04:45 +0200

    fixed gitignore

--- config/settings.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'defines.inc.php';
-
-define('_DB_SERVER_', 'localhost');
-define('_DB_PORT_', '3306');
-define('_DB_NAME_', 'local');
-define('_DB_USER_', 'local');
-define('_DB_PASSWD_', 'mine123');
